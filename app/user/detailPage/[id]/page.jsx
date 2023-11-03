25b8051507bf9c8ade1b63af12b111363118b6ed
highfee <odediranifeoluwa7@gmail.com>
2023-11-03 18:52:42 +0100

    populated data to the home page

--- app/user/detailPage/[id]/page.jsx
+++ app/user/detailPage/[id]/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import UserLayout from "@/app/layouts/userLayout";
+import { Button } from "@/components/ui";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Image from "next/image";
@@ -23,8 +24,8 @@ const NewsletterDetailPage = ({ params }) => {
   return (
     <div>
       <UserLayout>
-        <div className="max-w-container px-10 mx-auto py-y md:py-10 flex flex-col lg:flex-row gap-10 ">
-          <div className="border rounded-lg h-[300px] w-[300px] lg:sticky top-[102px] relative">
+        <div className="max-w-container px-2 md:px-10 mx-auto py-5 md:py-10 flex flex-col lg:flex-row gap-10 items-center lg:items-start">
+          <div className="border rounded-lg h-[300px] w-[300px] lg:stick ">
             <Image
               loader={() => imageLoader(data?.newsOwner.logoUrl)}
               src={data?.newsOwner.logoUrl}
@@ -34,8 +35,9 @@ const NewsletterDetailPage = ({ params }) => {
               className="h-full w-full object-cover"
               priority
             />
+            <Button className="w-full mt-4">Follow</Button>
           </div>
-          <div className="flex-1 borde rounded-lg ">
+          <div className="flex-1 borde rounded-lg mt-10 lg:mt-0 ">
             <div
               className="w- w-full p-5"
               dangerouslySetInnerHTML={{ __html: data?.res.data }}
